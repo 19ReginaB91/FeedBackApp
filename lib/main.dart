67ddb4a11f101c892b7c12d2be3454768fe8f45e
Regina Batiridou <r.i.v.batiridis@gmail.com>
2023-10-25 22:24:28 +0200

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
  WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp();
- runApp(FeedbackApp());
+ Firebase.initializeApp();
+ runApp(const FeedbackApp());
 }
 
 class FeedbackApp extends StatelessWidget {
